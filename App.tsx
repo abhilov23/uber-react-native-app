10f466417f0f4083897b0628a9b08158ff2dc221
Abhilov Gupta <113055824+abhilov23@users.noreply.github.com>
2025-10-31 13:11:32 +0530

    .

--- App.tsx
+++ App.tsx
@@ -1,12 +1,12 @@
-import { View, Text, StatusBar } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { View, Text } from 'react-native';
 import './global.css';
 
 export default function App() {
   return (
-    <View className='flex-1 items-center justify-center bg-white'> 
-        <Text className="text-2xl">Hello world</Text>
-        <StatusBar barStyle="dark-content" />
+    <View style={{ flex: 1, backgroundColor: 'yellow' }}> 
+        <View className="flex-1 items-center justify-center bg-red-500">
+          <Text className="text-2xl text-blue-500">Hello world</Text>
+        </View>
     </View>
   );
 }
