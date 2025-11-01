47283fcc7bc7ec507d85448674d0f738f7ac3e58
Abhilov Gupta <113055824+abhilov23@users.noreply.github.com>
2025-11-01 13:15:52 +0530

    .
    added the fonts and data

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,17 @@ module.exports = {
   ],
   presets: [require('nativewind/preset')],
   theme: {
-    extend: {},
+    extend: {
+       fontFamily: {
+        'jakarta-bold': ['Jakarta-Bold'],
+        'jakarta-extrabold': ['Jakarta-ExtraBold'],
+        'jakarta-extralight': ['Jakarta-ExtraLight'],
+        'jakarta-light': ['Jakarta-Light'],
+        'jakarta-medium': ['Jakarta-Medium'],
+        'jakarta': ['Jakarta-Regular'],
+        'jakarta-semibold': ['Jakarta-SemiBold'],
+      },
+    },
   },
   plugins: [],
 };
